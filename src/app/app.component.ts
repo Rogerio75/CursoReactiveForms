d8da299b3401fd0d3fe63296516c01cd3eeff973
rogerio75 <rogerio.ifg@gmail.com>
2023-10-10 16:52:47 -0300

    tarde

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<router-outlet></router-outlet>`,
+  template: `
+<!--
+<app-reactive-forms> </app-reactive-forms> -->
+  <router-outlet></router-outlet>`,
  })
 export class AppComponent {
   title = 'reactive-Forms';
